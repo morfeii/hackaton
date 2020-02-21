552f572d8b65ffc695570f8527f0bfd179abe885
Vitalii <vitaliybondarenko1982@gmail.com>
2020-02-21 22:37:05 +0200

    cleared repository

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.scss';
-import { MoviesList } from './components/MoviesList';
-import { NewMovie } from './components/NewMovie';
-import moviesFromServer from './api/movies.json';
 
-export class App extends Component {
-  state = {
-    movies: moviesFromServer,
-  };
-
-  addMovie = (movie) => {
-    // put your code here
-  };
-
-  render() {
-    const { movies } = this.state;
-
-    return (
-      <div className="page">
-        <div className="page-content">
-          <MoviesList movies={movies} />
-        </div>
-        <div className="sidebar">
-          <NewMovie />
-        </div>
-      </div>
-    );
-  }
-}
+export const App = () => (
+  <div className="app">
+    <h1 className>Mate Hackaton</h1>
+    <button type="button" className="btn btn-primary">Start</button>
+  </div>
+);
